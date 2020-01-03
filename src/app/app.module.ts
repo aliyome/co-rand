db4378b93cee033beb50bac1014727c0a173c2b6
aliyome <aliyome@gmail.com>
2020-01-04 03:44:07 +0900

    add room page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,17 +17,29 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireAuthGuardModule } from '@angular/fire/auth-guard';
+import {
+  AngularFireAuthGuardModule,
+  AngularFireAuthGuard,
+} from '@angular/fire/auth-guard';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from 'src/environments/environment';
 import { LoginComponent } from './login/login.component';
+import { RoomComponent } from './room/room.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, LoginComponent],
+  declarations: [AppComponent, NavComponent, LoginComponent, RoomComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'room/:id',
+        loadChildren: () =>
+          import('./room/room.module').then(m => m.RoomModule),
+        canActivate: [AngularFireAuthGuard],
+      },
+    ]),
     LayoutModule,
     MatToolbarModule,
     MatButtonModule,
